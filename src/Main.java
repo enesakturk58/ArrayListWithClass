060dfd718fd94e4586c9d7cd45a0644d442d8014
Enes Aktürk <enesakturk97@gmail.com>
2022-11-17 05:34:57 +0300

    Gereksiz kütüphaneler silindi.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,4 @@
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
 
